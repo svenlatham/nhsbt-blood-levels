ae8e95d3109ce6c737ae1241de86d8712871b549
Sven Latham <sven@noggin.bi>
2022-08-26 21:14:03 +0100

    Moving to a package-based achitecture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nhsbt_acquire',
+    version='0.1a',
+    description='Acquire and store NHSBT blood & platelet levels',
+    url='https://github.com/svenlatham/nhsbt-blood-levels/',
+    license='MIT',
+    author='Sven Latham',
+    author_email='sven@svenlatham.com',
+    packages='nhsbt_acquire'
+)
