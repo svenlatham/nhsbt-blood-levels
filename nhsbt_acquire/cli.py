e0fbb8f7de2108798faba3a421646ea754e6d7ea
Sven Latham <sven@noggin.bi>
2022-08-26 21:35:56 +0100

    Attempting parsing for the first time

--- nhsbt_acquire/cli.py
+++ nhsbt_acquire/cli.py
@@ -1,5 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 def main():
     r = requests.get("https://hospital.blood.co.uk/business-continuity/blood-stocks/")
-    print(r.text)
+    #print(r.text)
+    soup = BeautifulSoup(r.text)
+    texts = soup.select("div.grid-text")
+    for entry in texts:
+        print(entry.text)
