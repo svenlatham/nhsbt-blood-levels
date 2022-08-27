4fd6d0903ca1b4b2454889380fff93caeedec2c1
Sven Latham <sven@noggin.bi>
2022-08-27 22:08:35 +0100

    Attempting writes directly to CSV

--- nhsbt_acquire/cli.py
+++ nhsbt_acquire/cli.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import pandas
 
 def main():
     r = requests.get("https://hospital.blood.co.uk/business-continuity/blood-stocks/")
@@ -9,10 +10,19 @@ def main():
     texts = soup.select("div.grid-text")
     texts = [x for x in texts if 'Text version for accessibility' in x.text]
     f = open("nhsbt.txt","a")
-    for entry in texts:
+    # texts[0] contains blood; texts[1] contains platelets
+    # we need to check this in future to be sure!
+    blood = texts[0]
+    platelets = texts[1]
+    
+    for entry in blood:
         matches = re.findall('([OAB\+\-]+) ([0-9\.]+)', entry.text, re.DOTALL)
-        for match in matches:
-            # Presented as a tuple, so we can split that right up
-            str = ("%s,%s " % (match[0], match[1]))
-            f.write(str)
+        df = pandas.DataFrame.from_items(matches)
+        df.to_csv("blood.csv",mode='a', header=not os.path.exists('blood.csv'))
+    
+    for entry in platelets:
+        matches = re.findall('([OAB\+\-]+) ([0-9\.]+)', entry.text, re.DOTALL)
+        df = pandas.DataFrame.from_items(matches)
+        df.to_csv("platelets.csv",mode='a', header=not os.path.exists('platelets.csv'))
+
     f.close()
